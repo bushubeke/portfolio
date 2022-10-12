3045d1763f489217adfbd6fcbd0c04f392d803d1
bushubeke <beimdegefu@gmail.com>
2022-10-12 18:14:15 +0300

    oct 2022 bug fix

--- todosadmin/app/forms/__init__.py
+++ todosadmin/app/forms/__init__.py
@@ -2,4 +2,4 @@ from flask_wtf import FlaskForm,Form
 from flask_wtf.file import FileField, FileRequired
 from wtforms import StringField,PasswordField,SelectField,HiddenField
 from wtforms.validators import DataRequired,Email,AnyOf,EqualTo,ValidationError
-from wtforms.fields.html5 import DateField,DateTimeField
+from wtforms.fields import DateField,DateTimeField
